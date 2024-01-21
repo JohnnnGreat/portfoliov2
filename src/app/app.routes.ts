d31aa50db3173ccd850ffa484596f8fe161d71d0
John Ossai <johnossai20@gmail.com>
2024-01-21 06:31:28 -0800

    Worked on the services

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { animation } from '@angular/animations';
 import { slideInAnimation } from './route.animations';
 import { HomeComponent } from './home/home.component';
-import { MenuStateService } from './menu-state.service';
+import { MenuStateService } from 'c:/Users/John/portfoliov2/src/app/menu-state.service';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent, data: { title: 'Home Page' } },
