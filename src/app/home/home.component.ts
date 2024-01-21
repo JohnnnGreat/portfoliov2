b33549ff8bb6cf489884ccb88c37298a293cdf0d
John Ossai <johnossai20@gmail.com>
2024-01-21 07:14:12 -0800

    sd

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,11 +3,17 @@ import { HeroComponent } from './hero/hero.component';
 import { Title, Meta } from '@angular/platform-browser';
 import { ServicesComponent } from './services/services.component';
 import { TechstackComponent } from './techstack/techstack.component';
+import { AboutComponent } from './about/about.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeroComponent, ServicesComponent, TechstackComponent],
+  imports: [
+    HeroComponent,
+    ServicesComponent,
+    TechstackComponent,
+    AboutComponent,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
